6b312561efccaf8a12751815dd38f92aef4eb14d
Clrain <jin_hyeon_seo_01@naver.com>
2025-03-21 03:32:31 +0900

    [Test] Fix package

--- Assets/CustomShaderGraph/ConfigPath.cs
+++ Assets/CustomShaderGraph/ConfigPath.cs
@@ -4,6 +4,6 @@ public class ConfigPath
 {
     public const string rootPath = "Assets/CustomShaderGraph/";
     
-    public const string customUnlitForwardPassPath = rootPath + "Custom Tamplate/ShaderGraph/Includes/UnlitPass.hlsl";
-    public const string customUnlitGBufferPassPath = rootPath + "Custom Tamplate/ShaderGraph/Includes/UnlitGBufferPass.hlsl";
+    public const string customUnlitForwardPassPath = rootPath + "Custom Tamplate/Editor/ShaderGraph/Includes/UnlitPass.hlsl";
+    public const string customUnlitGBufferPassPath = rootPath + "Custom Tamplate/Editor/ShaderGraph/Includes/UnlitGBufferPass.hlsl";
 }
